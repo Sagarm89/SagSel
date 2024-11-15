3fd65267152f01e08a113c6dcf046a908dfdd196
Puja Jagani <puja.jagani93@gmail.com>
2024-11-15 14:47:30 +0530

    Fix build and formatting

--- dotnet/src/webdriver/Remote/RemoteWebDriver.cs
+++ dotnet/src/webdriver/Remote/RemoteWebDriver.cs
@@ -17,6 +17,7 @@
 // under the License.
 // </copyright>
 
+using OpenQA.Selenium.Internal.Logging;
 using OpenQA.Selenium.DevTools;
 using System;
 using System.Collections.Generic;
@@ -64,7 +65,7 @@ namespace OpenQA.Selenium.Remote
     public class RemoteWebDriver : WebDriver, IDevTools, IHasDownloads
     {
         private readonly ILogger _logger = Log.GetLogger(typeof(RemoteWebDriver));
-        
+
         /// <summary>
         /// The name of the Selenium grid remote DevTools end point capability.
         /// </summary>
@@ -430,8 +431,8 @@ namespace OpenQA.Selenium.Remote
             if (this.Capabilities.GetCapability(BrowserName) == "firefox")
             {
                 if (_logger.IsEnabled(LogEventLevel.Warn))
-                 {
-                _logger.Warn("CDP support for Firefox is deprecated and will be removed in future versions. Please switch to WebDriver BiDi.");
+                {
+                    _logger.Warn("CDP support for Firefox is deprecated and will be removed in future versions. Please switch to WebDriver BiDi.");
                 }
             }
 
