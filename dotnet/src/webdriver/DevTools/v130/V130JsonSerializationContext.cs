16431a87e1b30f0c53e5187b3da4de8d0dbe8e70
Michael Render <render.michael@gmail.com>
2025-01-28 17:19:12 -0500

    [dotnet] Generate JSON serializer per protocol

---
+++ dotnet/src/webdriver/DevTools/v130/V130JsonSerializationContext.cs
@@ -0,0 +1,6 @@
+using System.Text.Json.Serialization;
+
+namespace OpenQA.Selenium.DevTools.V130;
+
+[JsonSerializable(typeof(string))] // Placeholder
+internal sealed partial class V130JsonSerializationContext : JsonSerializerContext;
---
+++ dotnet/src/webdriver/DevTools/v131/V131JsonSerializationContext.cs
@@ -0,0 +1,6 @@
+using System.Text.Json.Serialization;
+
+namespace OpenQA.Selenium.DevTools.V131;
+
+[JsonSerializable(typeof(string))] // Placeholder
+internal sealed partial class V131JsonSerializationContext : JsonSerializerContext;
