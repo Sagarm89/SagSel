fbb1c736f7b4c83aed2e9134232bc31067486d6f
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2025-03-16 16:49:19 +0300

    [dotnet] [bidi] Make script `Target` as not nested

--- dotnet/src/webdriver/BiDi/Modules/Script/Target.cs
+++ dotnet/src/webdriver/BiDi/Modules/Script/Target.cs
@@ -21,16 +21,15 @@ using System.Text.Json.Serialization;
 
 namespace OpenQA.Selenium.BiDi.Modules.Script;
 
-[JsonDerivedType(typeof(Realm))]
-[JsonDerivedType(typeof(Context))]
-public abstract record Target
-{
-    public record Realm([property: JsonPropertyName("realm")] Script.Realm Target) : Target;
+[JsonDerivedType(typeof(RealmTarget))]
+[JsonDerivedType(typeof(ContextTarget))]
+public abstract record Target;
 
-    public record Context([property: JsonPropertyName("context")] BrowsingContext.BrowsingContext Target) : Target
-    {
-        public string? Sandbox { get; set; }
-    }
+public record RealmTarget([property: JsonPropertyName("realm")] Realm Target) : Target;
+
+public record ContextTarget([property: JsonPropertyName("context")] BrowsingContext.BrowsingContext Target) : Target
+{
+    public string? Sandbox { get; set; }
 }
 
 public class ContextTargetOptions
