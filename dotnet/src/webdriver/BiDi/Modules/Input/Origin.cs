0829cb31d82b9d609ae776399fce05bd950e7849
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2025-03-17 21:03:15 +0300

    [dotnet] [bidi] Make input `Origin` as not nested

--- dotnet/src/webdriver/BiDi/Modules/Input/Origin.cs
+++ dotnet/src/webdriver/BiDi/Modules/Input/Origin.cs
@@ -21,14 +21,14 @@ using System.Text.Json.Serialization;
 
 namespace OpenQA.Selenium.BiDi.Modules.Input;
 
-public abstract record Origin
+public abstract record Origin;
+
+public record ViewportOrigin() : Origin;
+
+public record PointerOrigin() : Origin;
+
+public record ElementOrigin(Script.ISharedReference Element) : Origin
 {
-    public record Viewport() : Origin;
-
-    public record Pointer() : Origin;
-
-    public record Element([property: JsonPropertyName("element")] Script.ISharedReference SharedReference) : Origin
-    {
-        public string Type { get; } = "element";
-    }
+    [JsonInclude]
+    internal string Type { get; } = "element";
 }
